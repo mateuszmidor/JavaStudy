7dbc3307c5c505c0daf61232a3ec62f0777da443
mateusz <kontakt@mateuszmidor.com>
2013-10-26 20:35:35 +0200

    JUnit for maven updated to ver 4.8 Java for maven updated to ver 1.7

--- RegularFractionsCalc/src/main/java/com/mateuszmidor/CoolCalc.java
+++ RegularFractionsCalc/src/main/java/com/mateuszmidor/CoolCalc.java
@@ -5,25 +5,23 @@ import java.util.StringTokenizer;
 public class CoolCalc implements Calculator {
 
 	// "2/5 + 1/5 * 4/3 - 2/3"
-	// presumption: there is at least one fractionin equation
+	// presumption: there is at least one fraction in equation
 	public Fraction calc(String equation) {
 		String cleanEquation = cleanTheEquation(equation);
-		
+
 		StringTokenizer st = new StringTokenizer(cleanEquation, "+-*", true);
-	
+
 		Fraction result = FractionParser.parse(st.nextToken());
 		while (st.hasMoreTokens()) {
 			String operator = st.nextToken(); // equation operator
 			Fraction fraction = FractionParser.parse(st.nextToken());
-			
+
 			result = doSimpleEquation(result, fraction, operator);
-			
 		}
 		return result;
 	}
 
-	private Fraction doSimpleEquation(Fraction a, Fraction b,
-			String operator) {
+	private Fraction doSimpleEquation(Fraction a, Fraction b, String operator) {
 
 		switch (operator) {
 		case "+":
